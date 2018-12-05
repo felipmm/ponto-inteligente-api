7f8ebfee8077a78b68e5df7096322c07bddd90f8
felipmm <felipmm@gmail.com>
2018-12-05 14:29:05 -0200

    Adicionado EhCache

--- src/main/java/com/fmm/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/fmm/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,11 +3,13 @@ package com.fmm.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
-//@SpringBootApplication
+@SpringBootApplication
+@EnableCaching
 // exclude foi colocado até ser criado o Websecurity para que o spring security exclua a
 //autenticacao padrao por enquanto
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
