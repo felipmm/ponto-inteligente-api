45cabacad1b12d60bc373b6533b22705c5d1ff51
felipmm <felipmm@gmail.com>
2018-12-05 11:29:22 -0200

    Atuaizações

---
+++ src/main/java/com/fmm/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,16 @@
+package com.fmm.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+//@SpringBootApplication
+// exclude foi colocado até ser criado o Websecurity para que o spring security exclua a
+//autenticacao padrao por enquanto
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+}
